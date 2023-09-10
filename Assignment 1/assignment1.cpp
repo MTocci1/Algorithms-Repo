1cc773f3ed964563b1704561b3c8d21fae1491ba
MTocci1 <Mason.Tocci1@marist.edu>
2023-09-10 17:12:59 -0400

    created assignment 1 project in Visual Studio Community

--- Assignment 1/assignment1.cpp
+++ Assignment 1/assignment1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
+int main() {
+    // Open the magicitems.txt file
+    std::ifstream inputFile("magicitems.txt");
+
+    // Check if file was successfully opened
+    if (!inputFile) {
+        std::cerr << "Error opening magicitems.txt" << std::endl;
+        return 1;
+    }
+
+    // Create vector to store the magic items
+    std::vector<std::string> magicItems;
+    std::string line;
+
+    // Read the file line-by-line
+    while (std::getline(inputFile, line)) {
+        magicItems.push_back(line);
+    }
+    // Close the file
+    inputFile.close();
+
+    // Print the contents
+    for (const std::string& item : magicItems) {
+        std::cout << item << std::endl;
+    }
+
+    return 0;
+}
