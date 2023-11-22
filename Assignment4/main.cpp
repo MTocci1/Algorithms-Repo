2a58be85dcc8b583b47fdabe050c76ed379a8a98
MTocci1 <Mason.Tocci1@marist.edu>
2023-11-22 15:34:54 -0500

    implemented bellman ford algorithm

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -5,85 +5,166 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
-#include <queue>
+#include <limits>
 
 using namespace std;
 
+class Vertex; 
+
 class Edge {
 public:
-    int starterID;
-    int destinationID;
+    Vertex* source;
+    Vertex* destination;
     int weight;
 
     // Constructor
-    Edge(int start, int dest, int w) : starterID(start), destinationID(dest), weight(w) {}
+    Edge(Vertex* s, Vertex* d, int w) : source(s), destination(d), weight(w) {}
 };
 
 class Vertex {
 public:
     int vertexID;
-    bool processed;
-    vector<Edge> outgoingEdges;
+    int distance;
+    Vertex* predecessor;
+    vector<Edge*> outgoingEdges;
 
     // Constructor
-    Vertex(int id) : vertexID(id), processed(false) {}
+    Vertex(int id) : vertexID(id), distance(numeric_limits<int>::max()), predecessor(nullptr) {}
 
     // Add an outgoing edge to the vertex
-    void addOutgoingEdge(int start, int dest, int weight) {
-        outgoingEdges.emplace_back(start, dest, weight);
+    void addOutgoingEdge(Vertex* dest, int weight) {
+        outgoingEdges.push_back(new Edge(this, dest, weight));
     }
 };
 
 class Graph {
 public:
-    vector<Vertex> vertices;
+    vector<Vertex*> vertices;
 
-    // Destructor to clear vertices
-    ~Graph() {
-        vertices.clear();
-    }
-
-    // Find a vertex by ID
     Vertex* findVertex(int id) {
-        for (auto& vertex : vertices) {
-            if (vertex.vertexID == id) {
-                return &vertex;
+        for (Vertex* vertex : vertices) {
+            if (vertex->vertexID == id) {
+                return vertex;
             }
         }
         return nullptr;
     }
 
+    Edge* findEdge(int sourceID, int destinationID) {
+        Vertex* source = findVertex(sourceID);
+        for (Edge* edge : source->outgoingEdges) {
+            if (edge->destination->vertexID == destinationID) {
+                return edge;
+            }
+        }
+        return nullptr;
+    }
+
+
     // Add a new vertex to the graph
     void addVertex(int id) {
-        vertices.push_back(Vertex(id));
+        vertices.push_back(new Vertex(id));
     }
 
     // Add a directed edge with weight between two vertices
-    void addDirectedEdge(int fromID, int toID, int weight) {
-        Vertex* from = findVertex(fromID);
-        Vertex* to = findVertex(toID);
+    void addDirectedEdge(Vertex* from, Vertex* to, int weight) {
+        from->addOutgoingEdge(to, weight);
+    }
 
-        if (from && to) {
-            from->addOutgoingEdge(fromID, toID, weight);
+    void printAllEdges() {
+        cout << "All Edges and Their Weights:\n";
+        for (Vertex* vertex : vertices) {
+            for (Edge* edge : vertex->outgoingEdges) {
+                cout << edge->source->vertexID << " --> " << edge->destination->vertexID << " weight is " << edge->weight << endl;
+            }
+        }
+        cout << endl;
+    }
+
+    // Initialize single source function
+    void initializeSingleSource(Vertex* source) {
+        for (Vertex* vertex : vertices) {
+            vertex->distance = numeric_limits<int>::max();
+            vertex->predecessor = nullptr;
+        }
+        source->distance = 0;
+    }
+
+    // Relax function
+    void relax(Vertex* u, Vertex* v, int weight) {
+        if (v->distance > u->distance + weight) {
+            v->distance = u->distance + weight;
+            v->predecessor = u;
         }
     }
 
-    void printAllEdges() const {
-        for (const auto& vertex : vertices) {
-            for (const auto& edge : vertex.outgoingEdges) {
-                cout << vertex.vertexID << " -> " << edge.destinationID << "; Weight = " << edge.weight << endl;
+    // Bellman-Ford algorithm
+    bool bellmanFord(Vertex* source) {
+        initializeSingleSource(source);
+
+        for (size_t i = 1; i <= vertices.size() - 1; ++i) {
+            for (Vertex* vertex : vertices) {
+                for (Edge* edge : vertex->outgoingEdges) {
+                    relax(edge->source, edge->destination, edge->weight);
+                }
+            }
+        }
+
+        // Print the shortest paths
+        cout << "Shortest paths from vertex " << source->vertexID << ":\n";
+        for (Vertex* vertex : vertices) {
+            if (vertex->distance == numeric_limits<int>::max()) {
+                cout << "Vertex " << vertex->vertexID << ": No path" << endl;
+            }
+            else {
+                printPath(source, vertex);
+                cout << endl;
+            }
+        }
+
+        return true;
+    }
+
+private:
+    // Print the path from source to destination
+    void printPath(Vertex* source, Vertex* destination) {
+        list<int> path;
+        while (destination != nullptr) {
+            path.push_front(destination->vertexID);
+            destination = destination->predecessor;
+        }
+
+        auto it = path.begin();
+        int prevVertexID = *it;
+        int totalCost = 0;
+        ++it;
+        for (; it != path.end(); ++it) {
+            Vertex* currentVertex = findVertex(*it);
+            totalCost += findEdge(prevVertexID, *it)->weight;
+            prevVertexID = *it;
+        }
+
+        // Print the starting and ending vertices
+        cout << source->vertexID << " --> " << *path.rbegin() << " cost is " << totalCost << "; path: ";
+
+        // Print the path
+        for (it = path.begin(); it != path.end(); ++it) {
+            cout << *it;
+            if (next(it) != path.end()) {
+                cout << " --> ";
             }
         }
     }
 };
 
 int main() {
+    Graph graph;
+
     // Open the file
     ifstream graphFile("graphs2.txt");
 
     // Handle failure
-    if (!graphFile)
-    {
+    if (!graphFile) {
         cerr << "Failed to open graphs1.txt" << endl;
         return 1;
     }
@@ -101,8 +182,10 @@ int main() {
         if (command == "new" && iss >> command && command == "graph") {
             // "new graph" command
             if (currentGraph) {
-                // Print all edges
+                // Print all edges before running Bellman-Ford
                 currentGraph->printAllEdges();
+                // Run Bellman-Ford algorithm
+                currentGraph->bellmanFord(currentGraph->vertices[0]);
                 delete currentGraph;
             }
             currentGraph = new Graph;
@@ -124,7 +207,9 @@ int main() {
                 int fromID, toID, weight;
                 char arrow;
                 if (iss >> fromID >> arrow >> toID >> weight && arrow == '-') {
-                    currentGraph->addDirectedEdge(fromID, toID, weight);
+                    Vertex* fromVertex = currentGraph->findVertex(fromID);
+                    Vertex* toVertex = currentGraph->findVertex(toID);
+                    currentGraph->addDirectedEdge(fromVertex, toVertex, weight);
                 }
             }
         }
@@ -132,8 +217,10 @@ int main() {
 
     // Print and delete the final graph
     if (currentGraph) {
-        // Print all edges
+        // Print all edges before running Bellman-Ford
         currentGraph->printAllEdges();
+        // Run Bellman-Ford algorithm
+        currentGraph->bellmanFord(currentGraph->vertices[0]);
         delete currentGraph;
     }
 
