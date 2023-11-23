7c8bf715f5a297a06bc0a0176835bdbb8f01f4b1
MTocci1 <Mason.Tocci1@marist.edu>
2023-11-23 16:34:59 -0500

    implemented fractional knapsack

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <iomanip>
 #include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -157,6 +158,63 @@ private:
     }
 };
 
+struct Spice {
+    string name;
+    double total_price;
+    int qty;
+    double unit_price;
+
+    Spice(string n = "", double tp = 0.0, int q = 0) : name(n), total_price(tp), qty(q) {
+        unit_price = total_price / qty;
+    }
+};
+
+struct Knapsack {
+    int capacity;
+    double worth;
+    vector<pair<string, double>> scoops;
+};
+
+bool compareSpicesByUnitPrice(const Spice& a, const Spice& b) {
+    return a.unit_price > b.unit_price;
+}
+
+void fractionalKnapsack(vector<Spice>& spices, vector<Knapsack>& knapsacks) {
+    // Sort spices by unit price in descending order
+    sort(spices.begin(), spices.end(), compareSpicesByUnitPrice);
+
+    // Fill knapsacks using fractional knapsack algorithm
+    for (auto& knapsack : knapsacks) {
+        int originalCapacity = knapsack.capacity;  // Store the original capacity
+
+        for (auto& spice : spices) {
+            if (spice.qty > 0) {
+                double quantity = min(static_cast<double>(knapsack.capacity), static_cast<double>(spice.qty));
+                knapsack.scoops.push_back({ spice.name, quantity });
+                knapsack.worth += quantity * spice.unit_price;
+                spice.qty -= static_cast<int>(quantity);
+                knapsack.capacity -= static_cast<int>(quantity);
+
+                if (knapsack.capacity == 0) {
+                    // Knapsack is full, move to the next one
+                    break; 
+                }
+            }
+        }
+
+        // Reset knapsack capacity to its original value
+        knapsack.capacity = originalCapacity;
+
+        // Reset spice quantities and unit prices after filling knapsack
+        for (auto& spice : spices) {
+            spice.qty = spice.total_price / spice.unit_price;
+
+            // Recalculate unit price after reset
+            spice.unit_price = spice.total_price / spice.qty;
+        }
+    }
+}
+
 int main() {
     Graph graph;
 
@@ -222,10 +280,64 @@ int main() {
         // Run Bellman-Ford algorithm
         currentGraph->bellmanFord(currentGraph->vertices[0]);
         delete currentGraph;
+        cout << endl;
     }
 
     // Close the file
     graphFile.close();
+
+    ifstream knapsackFile("spice.txt");
+
+    if (!knapsackFile.is_open()) {
+        cerr << "Failed to open spice.txt" << endl;
+        return 1;
+    }
+
+    vector<Spice> spices;
+    vector<Knapsack> knapsacks;
+
+    while (getline(knapsackFile, line)) {
+        if (line.find("spice name") != string::npos) {
+            size_t pos;
+
+            Spice spice;
+
+            pos = line.find("= ");
+            spice.name = line.substr(pos + 2, line.find(";") - pos - 2);
+
+            pos = line.find("total_price = ");
+            spice.total_price = stod(line.substr(pos + 14, line.find(";") - pos - 14));
+
+            pos = line.find("qty = ");
+            spice.qty = stoi(line.substr(pos + 6, line.find(";") - pos - 6));
+
+            spice.unit_price = spice.total_price / spice.qty;
+
+            spices.push_back(spice);
+        }
+        else if (line.find("knapsack capacity") != string::npos) {
+            Knapsack knapsack;
+            size_t pos = line.find("= ");
+            knapsack.capacity = stoi(line.substr(pos + 2, line.find(";") - pos - 2));
+            knapsack.worth = 0.0;
+
+            knapsacks.push_back(knapsack);
+        }
+    }
+
+    knapsackFile.close();
+
+    // Apply fractional knapsack algorithm
+    fractionalKnapsack(spices, knapsacks);
+
+    // Output the results
+    for (const auto& knapsack : knapsacks) {
+        cout << "Knapsack of capacity " << knapsack.capacity << " is worth " << knapsack.worth << " quatloos and contains ";
+        for (const auto& scoop : knapsack.scoops) {
+            cout << scoop.second << " scoops of " << scoop.first << ", ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
